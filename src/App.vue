660fe21f0d778d4b16154f3b07101a2cc41cb2bf
lilian <1220795758@qq.com>
2023-03-20 17:05:44 +0800

    起项

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="app">app组件</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue"
+import "reset.css"
+import "@/assets/css/index.less"
+import App from "./App.vue"
+
+createApp(App).mount("#app")
